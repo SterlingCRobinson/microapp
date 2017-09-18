a7fccd4a44b3f88f4a1b78453288f8bd9efede7e
sterling Robinson <sterlburst123@gmail.com>
2017-09-17 22:31:29 -0400

    migrated to get the schema

--- app.rb
+++ app.rb
@@ -2,3 +2,6 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/flash'
 require './models'
+
+set :database, {adapter: "sqlite3", database: "micro_blogging_app.sqlite3"}
+
---
+++ db/migrate/20170918015307_create_posts_table.rb
@@ -0,0 +1,10 @@
+class CreatePostsTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :posts do |t|
+  		t.string :content
+ 			t.string :username
+  		t.string :title
+    	t.integer :user_id
+  	end
+  end
+end
